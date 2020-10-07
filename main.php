76191fca2a70502fa908397c810a19d60c545d37
kkn <noah@twosun.com>
2020-10-07 16:45:46 +0900

    16:45

--- main.php
+++ main.php
@@ -1,5 +1,12 @@
-<html>
-test add
-vvv
- pc로 내용 수정 16시 34분
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+ </head>
+ <body>
+     
+ </body>
+ </html>
 </html>
